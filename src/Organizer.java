341b5e058bfdfd8f18f8b21941cf249e1096ca3e
DeclanGH <declanoonunkwo@gmail.com>
2023-10-26 09:57:06 -0400

    read JsonFile and added webscraper

--- src/Organizer.java
+++ src/Organizer.java
@@ -1,22 +1,37 @@
+import javax.json.*;
 import java.io.*;
+import java.util.Properties;
 
 public class Organizer {
 
     public static void main(String[] args) throws IOException {
-        CustomHashTable h1 = new CustomHashTable();
-        h1.add("mouse");
-        h1.add("mouse");
-        h1.add("is");
-        h1.add("going");
-        h1.add("to");
-        h1.add("beat");
-        h1.add("cat");
-        h1.add("is");
-        h1.add("up");
-        h1.add("the");
-        h1.add("cat");
-        h1.add("mouse");
 
-        h1.printAll();
+        // Properties
+        Properties properties = new Properties();
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream("config.properties");
+        }catch (FileNotFoundException e){
+            System.out.println("change the filepath in `config.properties` to match yours.");
+        }
+        properties.load(fis);
+
+        // GET json file location (change the .json file name below to match yours)
+        String filePath = properties.getProperty("filepath") + File.separator + "MyLinks.json";
+        File inputFile = new File(filePath);
+
+        // READ json file
+        InputStream inputStream = null;
+        try{
+            inputStream = new FileInputStream(inputFile);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        JsonReader reader = Json.createReader(inputStream);
+        JsonArray jArray = reader.readArray();
+        inputStream.close();
+        reader.close();
+
+
     }
 }
