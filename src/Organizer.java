8926ccad184a5989b5bb2982d5f2be0861bb0cec
DeclanGH <declanoonunkwo@gmail.com>
2023-10-28 16:36:58 -0400

    still testing some things

--- src/Organizer.java
+++ src/Organizer.java
@@ -1,20 +1,49 @@
 import javax.json.*;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class Organizer {
 
     public static void main(String[] args) throws Exception {
-        CustomHashTable ht = new CustomHashTable();
-        ht.add("man", "declan");
-        ht.add("woman", "grace");
-        ht.add("dog", "bruno");
-        ht.add("color", "red");
-        ht.add("season", "christmas");
-        ht.add("gpa", "3.7");
-        ht.add("country", "usa");
-        ht.add("color", "purple");
-        ht.printAll();
+        ArrayList<String> ht1 = new ArrayList<>();
+        ht1.add("this");
+        ht1.add("is");
+        ht1.add("the");
+        ht1.add("man");
+        ht1.add("that");
+        ht1.add("is");
+        ht1.add("coming");
+        ht1.add("here");
+
+        ArrayList<String> ht2 = new ArrayList<>();
+        ht2.add("this");
+        ht2.add("cow");
+        ht2.add("ate");
+        ht2.add("that");
+        ht2.add("man");
+        ht2.add("head");
+
+        ArrayList<String> ht3 = new ArrayList<>();
+        ht3.add("that");
+        ht3.add("man");
+        ht3.add("head");
+        ht3.add("look");
+        ht3.add("scrunchy");
+
+        CustomHashTable idfCounts = new CustomHashTable();
+        int count = 0;
+        for(String s : ht1){
+            idfCounts.countForIdf(s,1);
+        }
+        for(String s : ht2){
+            idfCounts.countForIdf(s,2);
+        }
+        for(String s : ht3){
+            idfCounts.countForIdf(s,3);
+        }
+
+        idfCounts.printAll();
 
        /* // Properties
         Properties properties = new Properties();
@@ -49,20 +78,22 @@ public class Organizer {
         WebScraper scraper = new WebScraper();
         CustomHashTable ht = new CustomHashTable();
 
-        CustomHashTable dictionary;
+        CustomHashTable dictionary = new CustomHashTable();
 
-        for(int i=0; i<arrayOfLinks.size(); i++){
+        for(int i=0; i<arrayOfLinks.size(); i++){ // populate dictionary
             String url = arrayOfLinks.getString(i);
 
             for(String s : scraper.webScrape(url)){
-                ht.add(s,12);
+                dictionary.countForIdf(s,i+1);
             }
-
-            String filePath = "/Users/declan/IdeaProjects/Wikipedia-Page-Similarity-II/src/records/" + url.substring(30);
-            File records = new File(filePath);
-            FileOutputStream fos = new FileOutputStream(filePath);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fos);
-            ht.writeObject(objectOutputStream);
         }
+
+        for(int i=0; i<arrayOfLinks.size(); i++){
+            String url = arrayOfLinks.getString(i);
+            for(String s : scraper.webScrape(url)){
+                ht.add(s,0);
+            }
+        }
+
     }
 }
