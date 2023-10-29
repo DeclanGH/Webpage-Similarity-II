ecccbf6113592830f9b141c10e620174cd398c3e
DeclanGH <declanoonunkwo@gmail.com>
2023-10-28 21:44:41 -0400

    ran test for serialization and deserialization (success)

--- src/Organizer.java
+++ src/Organizer.java
@@ -6,44 +6,88 @@ import java.util.Properties;
 public class Organizer {
 
     public static void main(String[] args) throws Exception {
-        ArrayList<String> ht1 = new ArrayList<>();
-        ht1.add("this");
-        ht1.add("is");
-        ht1.add("the");
-        ht1.add("man");
-        ht1.add("that");
-        ht1.add("is");
-        ht1.add("coming");
-        ht1.add("here");
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("this");
+        list1.add("is");
+        list1.add("the");
+        list1.add("man");
+        list1.add("that");
+        list1.add("is");
+        list1.add("coming");
+        list1.add("here");
 
-        ArrayList<String> ht2 = new ArrayList<>();
-        ht2.add("this");
-        ht2.add("cow");
-        ht2.add("ate");
-        ht2.add("that");
-        ht2.add("man");
-        ht2.add("head");
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("this");
+        list2.add("cow");
+        list2.add("ate");
+        list2.add("that");
+        list2.add("man");
+        list2.add("head");
 
-        ArrayList<String> ht3 = new ArrayList<>();
-        ht3.add("that");
-        ht3.add("man");
-        ht3.add("head");
-        ht3.add("look");
-        ht3.add("scrunchy");
+        ArrayList<String> list3 = new ArrayList<>();
+        list3.add("that");
+        list3.add("man");
+        list3.add("head");
+        list3.add("look");
+        list3.add("scrunchy");
 
-        CustomHashTable idfCounts = new CustomHashTable();
-        int count = 0;
-        for(String s : ht1){
-            idfCounts.countForIdf(s,1);
+        CustomHashTable dictionary = new CustomHashTable();
+        CustomHashTable doc1 = new CustomHashTable();
+        CustomHashTable doc2 = new CustomHashTable();
+        CustomHashTable doc3 = new CustomHashTable();
+
+        for(String s : list1){
+            dictionary.countForIdf(s,1);
         }
-        for(String s : ht2){
-            idfCounts.countForIdf(s,2);
+        for(String s : list2){
+            dictionary.countForIdf(s,2);
         }
-        for(String s : ht3){
-            idfCounts.countForIdf(s,3);
+        for(String s : list3){
+            dictionary.countForIdf(s,3);
         }
 
-        idfCounts.printAll();
+        for(String s : list1) {
+            double preIdf = 3.0/dictionary.getIdfCount(s);
+            doc1.advancedAdd(s,list1.size(),preIdf);
+        }
+        for(String s : list2) {
+            double preIdf = 3.0/dictionary.getIdfCount(s);
+            doc2.advancedAdd(s,list2.size(),preIdf);
+        }
+        for(String s : list3) {
+            double preIdf = 3.0/dictionary.getIdfCount(s);
+            doc3.advancedAdd(s,list3.size(),preIdf);
+        }
+
+        doc1.printAll();
+        doc2.printAll();
+        doc3.printAll();
+
+        FileOutputStream fos = new FileOutputStream("doc1");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(doc1);
+        FileOutputStream fos2 = new FileOutputStream("doc2");
+        ObjectOutputStream oos2 = new ObjectOutputStream(fos2);
+        oos2.writeObject(doc2);
+        FileOutputStream fos3 = new FileOutputStream("doc3");
+        ObjectOutputStream oos3 = new ObjectOutputStream(fos3);
+        oos3.writeObject(doc3);
+
+        FileInputStream fis = new FileInputStream("doc1");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        CustomHashTable stream1 = (CustomHashTable) ois.readObject();
+        FileInputStream fis2 = new FileInputStream("doc2");
+        ObjectInputStream ois2 = new ObjectInputStream(fis2);
+        CustomHashTable stream2 = (CustomHashTable) ois2.readObject();
+        FileInputStream fis3 = new FileInputStream("doc3");
+        ObjectInputStream ois3 = new ObjectInputStream(fis3);
+        CustomHashTable stream3 = (CustomHashTable) ois3.readObject();
+
+        stream1.printAll();
+        stream2.printAll();
+        stream3.printAll();
+
+
 
        /* // Properties
         Properties properties = new Properties();
@@ -91,7 +135,7 @@ public class Organizer {
         for(int i=0; i<arrayOfLinks.size(); i++){
             String url = arrayOfLinks.getString(i);
             for(String s : scraper.webScrape(url)){
-                ht.add(s,0);
+
             }
         }
 
