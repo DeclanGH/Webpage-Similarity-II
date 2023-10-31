81b8ab0ed4961428f09df7e06517d6f18cb24cba
DeclanGH <declanoonunkwo@gmail.com>
2023-10-31 02:44:20 -0400

    initial SimilarityAlgorithm

--- src/SimilarityAlgorithm.java
+++ src/SimilarityAlgorithm.java
@@ -5,16 +5,15 @@
  * Fall 2023
  */
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
+import Generators.WebScraper;
+import HashClasses.CustomHashTable;
+import HashClasses.ExtendibleHashing;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.io.*;
-import java.util.Properties;
 
 public class SimilarityAlgorithm {
     private JFormattedTextField userInput;
@@ -31,20 +30,41 @@ public class SimilarityAlgorithm {
         return this.panel;
     }
 
-    public SimilarityAlgorithm() {
+    public SimilarityAlgorithm() throws IOException, ClassNotFoundException {
+
+        // Deserialize before listeners
+
+        // Deserialize Extensible hashing class
+        FileInputStream fis = new FileInputStream("SerializedExtensibleHashingClass");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        ExtendibleHashing urlsMappedToObject = (ExtendibleHashing) ois.readObject();
+
+        // Deserialize Dictionary
+        FileInputStream fis1 = new FileInputStream("SerializedDictionary");
+        ObjectInputStream ois1 = new ObjectInputStream(fis1);
+        CustomHashTable dictionary = (CustomHashTable) ois1.readObject();
+
+        // Deserialize Dictionary
+        FileInputStream fis2 = new FileInputStream("SerializedUrlList");
+        ObjectInputStream ois2 = new ObjectInputStream(fis2);
+        CustomHashTable deserializedUrlList = (CustomHashTable) ois2.readObject();
+        String[] myUrls = deserializedUrlList.toKeyList();
+
         runButton.addActionListener(e -> {
             try {
                 String userInput = this.userInput.getText();
-                compareLinks(userInput);
-            } catch (IOException ex) {
+                findTwoMostSimilar(userInput, urlsMappedToObject, dictionary, myUrls);
+            } catch (IOException | ClassNotFoundException ex) {
                 throw new RuntimeException(ex);
             }
         });
+
         copyButton1.addActionListener(e -> {
             StringSelection stringSelection = new StringSelection(outputLink1.getText());
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             clipboard.setContents(stringSelection,null);
         });
+
         copyButton2.addActionListener(e -> {
             StringSelection stringSelection = new StringSelection(outputLink2.getText());
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
@@ -52,87 +72,89 @@ public class SimilarityAlgorithm {
         });
     }
 
-    private static JsonObject readFile() throws IOException {
+    private void findTwoMostSimilar
+            (String userInput, ExtendibleHashing urlsMappedToObject, CustomHashTable dictionary, String[] myUrls)
+            throws IOException, ClassNotFoundException {
 
-        // Properties
-        Properties properties = new Properties();
-        FileInputStream fis = null;
-        try{
-            fis = new FileInputStream("config.properties");
-        }catch (FileNotFoundException e){
-            System.out.println("change the filepath in `config.properties` to match yours.");
+        WebScraper ws = new WebScraper();
+        String[] userLinkWords = ws.webScrape(userInput);
+
+        for(String s : userLinkWords){
+            dictionary.countForIdf(s,201); // its 201 because this is the 201 url
         }
-        properties.load(fis);
 
-        // GET json file location (change the .json file name below to match yours)
-        String filePath = properties.getProperty("filepath") + File.separator + "MyLinks.json";
-        File inputFile = new File(filePath);
+        CustomHashTable userUrlHashTable = new CustomHashTable();
 
-        // READ json file
-        InputStream inputStream = null;
-        try{
-            inputStream = new FileInputStream(inputFile);
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
+        for(String s : userLinkWords){
+            double idf = (double) userLinkWords.length / dictionary.getIdfCount(s);
+            userUrlHashTable.advancedAdd(s,userLinkWords.length,idf);
         }
-        JsonReader reader = Json.createReader(inputStream);
-        JsonObject object = reader.readObject();
-        inputStream.close();
-        reader.close();
 
-        return object;
-    }
-
-    private void compareLinks(String userInput) throws IOException {
-        WebScraper ws1 = new WebScraper();
-        WebScraper ws2 = new WebScraper();
+        double currSimilarityScore = 0;
 
         // initialize with a random low score less than zero
         double maxSimilarity = -1000;
         double maxSimilarity2 = -500;
-        int indexOfMaxSimilarity = -1;
-        int indexOfMaxSimilarity2 = -1;
+        String urlOfMax = "";
+        String urlOfMax2 = "";
 
-        JsonArray arrayOfLinks = null;
-        try {
-            arrayOfLinks = readFile().getJsonArray("Links");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        for(int i=0; i<myUrls.length; i++){
+            ByteArrayInputStream bis = new ByteArrayInputStream(urlsMappedToObject.find(myUrls[i]));
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            CustomHashTable myUrlHashTable = (CustomHashTable) ois.readObject();
+            String[] myUrlWordList = myUrlHashTable.toKeyList();
 
-        CustomHashTable ht = new CustomHashTable();
-        String[] userLinkAsArray = ws1.webScrape(userInput);
-        for(String s : userLinkAsArray){
-            ht.add(s);
-        }
-        Object[] userLinkAsSet = ht.toKeyList();
+            for(String s : myUrlWordList){
+                double idf = (double) (myUrls.length + 1) / dictionary.getIdfCount(s);
+                myUrlHashTable.setIdf(s,idf);
+            }
 
-        for(int i=0; i<10; i++){
-            String[] wordsInMyLinks = ws2.webScrape(arrayOfLinks.getString(i));
-            double currSimilarityScore = doCosineSimilarity(userLinkAsSet,wordsInMyLinks);
+            currSimilarityScore = doCosineSimilarity(userUrlHashTable, myUrlHashTable,myUrlWordList);
+
             if(currSimilarityScore > maxSimilarity2){ // score must be greater than the smallest max to be considered
                 maxSimilarity2 = currSimilarityScore;
-                indexOfMaxSimilarity2 = i;
+                urlOfMax2 = myUrls[i];
                 if(maxSimilarity2 > maxSimilarity){ // swap if greater than our main max (maxSimilarity)
                     double temp = maxSimilarity2;
                     maxSimilarity2 = maxSimilarity;
                     maxSimilarity = temp;
-                    int temp1 = indexOfMaxSimilarity2; // swap indexes too...
-                    indexOfMaxSimilarity2 = indexOfMaxSimilarity;
-                    indexOfMaxSimilarity = temp1;
+                    String temp1 = urlOfMax2; // swap Strings too...
+                    urlOfMax2 = urlOfMax;
+                    urlOfMax = temp1;
                 }
             }
+
+
         }
 
         // Convert to string and display
         textPane1.setText((int)(maxSimilarity * 100)+"%");
         textPane2.setText((int)(maxSimilarity2 * 100)+"%");
 
-        outputLink1.setText(arrayOfLinks.getString(indexOfMaxSimilarity));
-        outputLink2.setText(arrayOfLinks.getString(indexOfMaxSimilarity2));
+        outputLink1.setText(urlOfMax);
+        outputLink2.setText(urlOfMax2);
     }
 
-    private static double doCosineSimilarity(Object [] a, String[] b){
+    private static double doCosineSimilarity
+            (CustomHashTable userHt, CustomHashTable myHt, String[] myUrlWordList){
 
+       // cosine similarity = A . B / (|A| * |B|), where A and B are vectors
+
+        // numerator variables
+        double a;
+        double numerator = 0;
+
+        // denominator variables
+        double b = 0;
+        double c = 0;
+
+        for(String s : myUrlWordList){
+            a = userHt.getTfidf(s) * myHt.getTfidf(s);
+            b += (userHt.getTfidf(s) * userHt.getTfidf(s));
+            c += (myHt.getTfidf(s) * myHt.getTfidf(s));
+            numerator += a;
+        }
+
+        return numerator/((Math.sqrt(b)) * (Math.sqrt(c)));
     }
 }
